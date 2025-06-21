b344e884b293ffbdd2325c1ad7d49b0d8163934f
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-21 17:59:20 -0500

    feat(model): 👤 define User model with validations and Sequelize options

---
+++ Usersmanagement/auth-jwt/src/models/user.model.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db.config");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    correo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    contraseña: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false, // ✅ desactiva createdAt y updatedAt
+    tableName: "Users",
+  }
+);
+
+module.exports = User;
