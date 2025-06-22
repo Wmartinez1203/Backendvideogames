5a2672c76ee13fe8de2956c7d3b68f40356c9971
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-22 16:36:18 -0500

    test(core): ✅ add EditGameApplicationTests for basic context load

---
+++ Catalog/edit-game/src/test/java/com/project/editgame/EditGameApplicationTests.java
@@ -0,0 +1,11 @@
+package com.project.editgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditGameApplicationTests {
+	@Test
+	void contextLoads() {}
+}
+
