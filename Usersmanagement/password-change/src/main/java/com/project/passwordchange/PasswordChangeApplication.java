6abeb89385844293cff44dd9280ad94eae20994c
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-21 21:27:11 -0500

    feat(app): 🚀 create main PasswordChangeApplication class

---
+++ Usersmanagement/password-change/src/main/java/com/project/passwordchange/PasswordChangeApplication.java
@@ -0,0 +1,13 @@
+package com.project.passwordchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordChangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordChangeApplication.class, args);
+	}
+}
+
