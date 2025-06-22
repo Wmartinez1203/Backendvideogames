5dcfef68f351572f302c0f2a4fec16235893e59b
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-21 20:01:00 -0500

    refactor(model): 🎨 change User ID from UUID to auto-incrementing INTEGER

--- Usersmanagement/create-user/src/models/user.model.js
+++ Usersmanagement/create-user/src/models/user.model.js
@@ -3,8 +3,8 @@ const sequelize = require("../config/db.config");
 
 const User = sequelize.define("User", {
   id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
+    type: DataTypes.INTEGER,          // ✅ Cambiado a INTEGER
+    autoIncrement: true,              // ✅ Auto incremental
     primaryKey: true
   },
   nombre: {
