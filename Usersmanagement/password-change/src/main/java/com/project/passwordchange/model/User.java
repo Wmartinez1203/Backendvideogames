de23b80f4ecf195e1448b4e8fbb4fb46720e8a32
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-21 21:28:01 -0500

    feat(model): 🗃️ define User entity mapped to "Users" table and JPA repository

---
+++ Usersmanagement/password-change/src/main/java/com/project/passwordchange/model/User.java
@@ -0,0 +1,24 @@
+package com.project.passwordchange.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "\"Users\"") // Respeta mayúsculas
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @Column(unique = true, nullable = false)
+    private String correo;
+
+    private String contraseña;
+}
