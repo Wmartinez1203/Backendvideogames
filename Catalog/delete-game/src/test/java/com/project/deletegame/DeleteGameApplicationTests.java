7ae69574609b5ecd2b49755ba2a1113ce022f48a
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-22 17:02:43 -0500

    test(app): ✅ add DeleteGameApplicationTests class

---
+++ Catalog/delete-game/src/test/java/com/project/deletegame/DeleteGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.deletegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleteGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
