e83f34c9ec243d81311a6bb3f4b3b4754d450a1a
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-21 21:27:52 -0500

    feat(controller): 🧩 add password change controller supporting JSON payload

---
+++ Usersmanagement/password-change/src/main/java/com/project/passwordchange/dto/PasswordChangeRequest.java
@@ -0,0 +1,10 @@
+package com.project.passwordchange.dto;
+
+import lombok.Data;
+
+@Data
+public class PasswordChangeRequest {
+    private Long userId;
+    private String oldPassword;
+    private String newPassword;
+}
