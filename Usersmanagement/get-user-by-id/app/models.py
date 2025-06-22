37e7b57c876188a8fb2fb4e4dad4725e70e98885
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-21 20:19:45 -0500

    refactor(model): 🎨 change User ID from UUID to INTEGER autoincrement

--- Usersmanagement/get-user-by-id/app/models.py
+++ Usersmanagement/get-user-by-id/app/models.py
@@ -1,13 +1,11 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import declarative_base
-import uuid
-from sqlalchemy.dialects.postgresql import UUID
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = "Users"
 
-    id = sa.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)  # ✅ ID como INTEGER
     nombre = sa.Column(sa.String, nullable=False)
     correo = sa.Column(sa.String, unique=True, nullable=False)
