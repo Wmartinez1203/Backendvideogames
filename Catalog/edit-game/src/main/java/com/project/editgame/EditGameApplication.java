1512b07bd6c6fae55823ceaf7fb51ea85b891f60
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-22 16:34:55 -0500

    feat(core): 🎉 add EditGameApplication as Spring Boot entry point

---
+++ Catalog/edit-game/src/main/java/com/project/editgame/EditGameApplication.java
@@ -0,0 +1,11 @@
+package com.project.editgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditGameApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EditGameApplication.class, args);
+	}
+}
