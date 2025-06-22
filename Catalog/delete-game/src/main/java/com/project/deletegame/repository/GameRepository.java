b79532ad9f26b12d481eda371a556f3ff21a9db9
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-22 17:02:08 -0500

    feat(repository): 💾 add GameRepository interface

---
+++ Catalog/delete-game/src/main/java/com/project/deletegame/repository/GameRepository.java
@@ -0,0 +1,7 @@
+package com.project.deletegame.repository;
+
+import com.project.deletegame.model.Game;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface GameRepository extends MongoRepository<Game, String> {
+}
