acedc970b2a61b6dc8752661a6c7b5e05138d8d4
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-22 15:49:38 -0500

    test(app): ✅ add test to verify Spring Boot application context loads correctly

---
+++ Catalog/register-new-game/src/test/java/com/project/registernewgame/RegisterNewGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.registernewgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterNewGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
