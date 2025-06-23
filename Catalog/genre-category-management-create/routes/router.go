21037a95e78121dd9685d242b7288ccc7d531752
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-22 21:06:35 -0500

    feat(router): 🛣️ configure routes for genre management service

---
+++ Catalog/genre-category-management-create/routes/router.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"net/http"
+
+	"genre-category-management/handlers"
+
+	"github.com/gorilla/mux"
+)
+
+func SetupRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/genres", handlers.CreateGenre).Methods("POST")
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("🎮 Genre Category Management Service Running"))
+	})
+
+	return r
+}
