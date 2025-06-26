f1677fab62558254072f60cfa944aaa56fed0037
Fernando Martínez <wladimirmartinez1203@gmail.com>
2025-06-25 22:08:30 -0500

    chore(db): 🗃 setup async SQLAlchemy connection with PostgreSQL in db.py

--- Usersmanagement/get-user-by-id/app/db.py
+++ Usersmanagement/get-user-by-id/app/db.py
@@ -5,7 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+PG_USER = os.getenv("PG_USER")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
+PG_HOST = os.getenv("PG_HOST")
+PG_PORT = os.getenv("PG_PORT")
+PG_DATABASE = os.getenv("PG_DATABASE")
+
+DATABASE_URL = f"postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DATABASE}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
