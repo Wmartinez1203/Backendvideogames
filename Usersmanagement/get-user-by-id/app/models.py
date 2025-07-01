ab0982449c4f1f099f8950b76c190b358b6d4afe
Fernando Martínez <134228921+Wmartinez1203@users.noreply.github.com>
2025-06-30 23:47:51 -0500

    feat(models): 🗃️ create User SQLAlchemy model with id, nombre, correo fields

--- Usersmanagement/get-user-by-id/app/models.py
+++ Usersmanagement/get-user-by-id/app/models.py
@@ -6,6 +6,6 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
 
-    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)  # ✅ ID como INTEGER
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     nombre = sa.Column(sa.String, nullable=False)
     correo = sa.Column(sa.String, unique=True, nullable=False)
