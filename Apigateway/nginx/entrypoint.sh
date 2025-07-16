11d520bec2910055c8201a83eae86879a1c0f7e5
Fernando Martínez <134228921+Wmartinez1203@users.noreply.github.com>
2025-07-15 20:55:17 -0500

    feat(Apigateway): 🔧 update NGINX configuration and environment variables for new services

--- Apigateway/nginx/entrypoint.sh
+++ Apigateway/nginx/entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 
 echo "📦 Generando archivo de configuración NGINX..."
 
-envsubst < /etc/nginx/default.template.conf > /etc/nginx/conf.d/default.conf
+envsubst '$CREATE_USER_HOST $CREATE_USER_PORT $DELETE_USER_HOST $DELETE_USER_PORT $UPDATE_USER_HOST $UPDATE_USER_PORT $REGISTER_GAME_HOST $REGISTER_GAME_PORT $GRAPHQL_USER_HOST $GRAPHQL_USER_PORT $SOAP_CREATE_ROLE_HOST $SOAP_CREATE_ROLE_PORT $SOAP_DELETE_ROLE_HOST $SOAP_DELETE_ROLE_PORT $SOAP_UPDATE_ROLE_HOST $SOAP_UPDATE_ROLE_PORT $SOAP_GETBYID_ROLE_HOST $SOAP_GETBYID_ROLE_PORT $SOAP_GETALL_ROLE_HOST $SOAP_GETALL_ROLE_PORT' \
+< /etc/nginx/default.template.conf > /etc/nginx/conf.d/default.conf
+
+echo "✅ Configuración generada correctamente en /etc/nginx/conf.d/default.conf"
 
 echo "🚀 Iniciando NGINX..."
 
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
