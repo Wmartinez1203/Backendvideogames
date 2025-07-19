7b6a3f4c0314f1a44ae2f8bad97e0925815bbdc3
Fernando Martínez <134228921+Wmartinez1203@users.noreply.github.com>
2025-07-19 10:56:46 -0500

    fix() : 🐛update routes

--- Usersmanagement/delete-user/src/routes/user.routes.js
+++ Usersmanagement/delete-user/src/routes/user.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { eliminarUsuario } = require("../controllers/user.controller");
 const verificarToken = require("../middleware/auth");
 
-router.delete("/usuarios/:id", eliminarUsuario);
+router.delete("/usuarios/delete/:id", eliminarUsuario);
 
 module.exports = router;
